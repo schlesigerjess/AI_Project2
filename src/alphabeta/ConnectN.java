f54099b08dab34e6262574ac91a5db6b6245a9ef
Jessica Schlesiger <Schlesigerjess@gmail.com>
2016-10-29 13:37:34 -0400

    Test Cases for constructer

--- src/alphabeta/ConnectN.java
+++ src/alphabeta/ConnectN.java
@@ -11,32 +11,28 @@ public class ConnectN {
 	public ConnectN (int size) {
 		this.size=size;
 		board = new char[(2*(size-1))][(2*size-1)];
-		for (int r = 0; r < size; r++)
-			for (int c = 0; c < size; c++)
+		for (int r = 0; r < (2*(size-1)); r++) // Rows
+			for (int c = 0; c < (2*size-1); c++) // Column
 				board[r][c] = ' ';
-
 		lastPlayed = 'R';
-
-
 	}
 
 
 	/** Takes in a current board state **/
 	public ConnectN(char board[][])
 	{
-		this.size = board.length;
-		board = new char[(2*(size-1))][(2*size-1)];
+		this.size = (board.length/2)+1;
+		this.board = new char[(2*(size-1))][(2*size-1)];
 		int numB = 0, numR = 0;
 
-		for (int r = 0; r < size; r++)
-			for (int c = 0; c < size; c++) {
+		for (int r = 0; r < (2*(size-1)); r++) // Rows
+			for (int c = 0; c < (2*size-1); c++) { // Column
 				this.board[r][c] = board[r][c];
 				if (board[r][c] == 'B') numB++;
 				if (board[r][c] == 'R') numR++;
 			}
 
 		this.depth = numB + numR;
-
 		if (numB == numR) this.lastPlayed = 'R';
 		else if ((numB - numR) == 1) this.lastPlayed = 'B';
 		else throw new RuntimeException("Error - invalid board state");
---
+++ test/alphabeta/testConnectN.java
@@ -0,0 +1,69 @@
+package alphabeta;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/** This tests our ConnectN class **/
+public class testConnectN {
+
+	/**
+	 * @Author Jessica Schlesiger
+	 * Tests passing in an already created board
+	 */
+	@Test
+	public void testPassCurrentBoard() {
+
+		char board[][] = {							// Black's turn
+				{ ' ', 'R', ' ', ' ', ' ', ' ', 'B' },
+				{ ' ', 'B', 'R', ' ', ' ', ' ', ' ' },
+				{ ' ', 'B', ' ', ' ', ' ', 'R', ' ' },
+				{ ' ', ' ', 'R', ' ', ' ', ' ', ' ' },
+				{ ' ', 'B', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', ' ', 'R', 'B', ' ', ' ', 'B' },
+		};
+		ConnectN c = new ConnectN(board);
+		assertEquals('B', c.lastPlayed );
+		char board2[][] = { 						// Red's turn
+				{ ' ', 'R', ' ', ' ', ' ', ' ', 'B' },
+				{ ' ', 'B', 'R', ' ', ' ', ' ', ' ' },
+				{ ' ', 'B', ' ', ' ', ' ', 'R', ' ' },
+				{ 'R', ' ', 'R', ' ', ' ', ' ', ' ' },
+				{ ' ', 'B', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', ' ', 'R', 'B', ' ', ' ', 'B' },
+		};
+		c = new ConnectN(board2);
+		assertEquals('R', c.lastPlayed );
+
+		try {
+	    char board3[][] = { 						// Invalid State
+				{ ' ', 'R', ' ', ' ', ' ', ' ', 'B' },
+				{ ' ', 'B', 'R', ' ', ' ', ' ', ' ' },
+				{ ' ', 'B', ' ', ' ', ' ', 'R', ' ' },
+				{ 'R', ' ', 'R', ' ', ' ', ' ', ' ' },
+				{ ' ', 'B', ' ', ' ', ' ', 'B', ' ' },
+				{ ' ', ' ', 'R', 'B', ' ', 'B', 'B' },
+		};
+		c = new ConnectN(board3);
+		fail("Should have failed");
+		} catch (final Exception e) {
+			final String msg = "Error - invalid board state";
+			assertEquals(msg, e.getMessage());
+
+		}
+
+	}
+
+
+	/**
+	 * @Author Jessica Schlesiger
+	 * Creates an empty board of N size
+	 */
+	@Test
+	public void testCreateBlankBoard() {
+		for (int size=4;size<20;size++) {
+		ConnectN c = new ConnectN(size);
+		assertEquals(2*size-1, c.board[0].length); // Checks column
+		assertEquals(2*(size-1), c.board.length); // Checks row
+		}
+	}
+
+}
