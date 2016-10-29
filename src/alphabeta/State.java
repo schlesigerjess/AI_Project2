3ac11e3a150bea3a615a5234c12f602f6e5a2061
Jessica Schlesiger <Schlesigerjess@gmail.com>
2016-10-29 12:50:56 -0400

    Given Code for Alpha Beta

---
+++ src/alphabeta/State.java
@@ -0,0 +1,15 @@
+package alphabeta;
+
+import java.util.LinkedList;
+
+public interface State {
+
+
+	public boolean isTerminal( );
+	LinkedList<State> next( );
+	public int utility( );
+	int getDepth( );
+	char getPlayer( );
+	public int evaluate();
+	public LinkedList<State> getNext();
+}
